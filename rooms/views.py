ec46c899b7fea48620ae1e6f8afc925303eb6a9d
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2020-12-28 02:32:32 +0900

    change rooms.views fbv -> cbv

--- rooms/views.py
+++ rooms/views.py
@@ -1,29 +1,13 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page_size = 10
+class HomeView(ListView):
 
-    # 장고 어시스트
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, page_size)
-    rooms = paginator.get_page(page)
+    """ HomeView Definition """
 
-    # Room 뒤집기
-    page = int(page)
-    limit = page_size * page
-    offest = limit - page_size
-    reversed_rooms = reversed(models.Room.objects.get_queryset())
-    lastest_rooms = []
-    for room in reversed_rooms:
-        lastest_rooms.append(room)
-    all_rooms = lastest_rooms[offest:limit]
-
-    return render(
-        request,
-        "rooms/home.html",
-        context={"page": rooms, "lastset_rooms": all_rooms},
-    )
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 2
+    context_object_name = "rooms"
+    ordering = ("-created",)
